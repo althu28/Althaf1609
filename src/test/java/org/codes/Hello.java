16b9ff1dc9ffcf9339f4f6c22bb4dd006bcd8010
althaf28 <althafprofessionaluse@gmail.com>
2024-09-16 18:14:39 +0530

    code added

---
+++ src/test/java/org/codes/Hello.java
@@ -0,0 +1,13 @@
+package org.codes;
+
+import org.base.BaseClass;
+
+public class Hello extends BaseClass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("VANNAKA DA MAPLA");
+		
+	}
+
+}
